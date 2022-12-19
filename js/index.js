2c367dbbe341b9906feee8c86a9c7c646ee03f74
alex traverso <alextraverso6@gmail.com>
2022-12-19 09:08:01 -0300

    fix features img url

--- js/index.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = function () {
-    let position = window.pageYOffset() || document.documentElement.scrollTop;
-
-    let laptop = document.getElementById("laptopBanner");
-
-    laptop.style.top = position * 0.1 + 'px';
-
-}
